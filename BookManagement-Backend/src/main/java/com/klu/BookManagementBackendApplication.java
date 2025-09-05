850d1f4d4bac773af35a13a81a20a6faf98abcc9
KHVS <2300032396@kluniversity.in>
2025-09-05 19:55:17 +0530

    Changed

--- BookManagement-Backend/src/main/java/com/klu/BookManagementBackendApplication.java
+++ BookManagement-Backend/src/main/java/com/klu/BookManagementBackendApplication.java
@@ -2,9 +2,10 @@ package com.klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookManagementBackendApplication {
+public class BookManagementBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookManagementBackendApplication.class, args);
